f60f64ba66db73f3ba19009cd0b606b0eeebeafb
Angela Omondi <omondiangela426@gmail.com>
2025-09-12 21:12:35 +0300

    Refactor index and signin pages to use require_once for ClassAutoLoad and streamline layout calls

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <?php
-require 'ClassAutoLoad.php';
-
-$ObjLayout->header($conf);
-$ObjLayout->nav($conf);
-$ObjLayout->banner($conf);
-$ObjLayout->content($conf);
-$ObjLayout->footer($conf);
+// Include the ClassAutoLoad Method
+require_once 'ClassAutoLoad.php';
+$layout->header($conf);
+print $hello->today();
+$form->signup();
+$layout->footer($conf);
--- signin.php
+++ signin.php
@@ -1,8 +1,7 @@
 <?php
-require 'ClassAutoLoad.php';
-
-$ObjLayout->header($conf);
-$ObjLayout->nav($conf);
-$ObjLayout->banner($conf);
-$ObjLayout->form_content($conf, $ObjForm);
-$ObjLayout->footer($conf);
+// Include the ClassAutoLoad Method
+require_once 'ClassAutoLoad.php';
+$layout->header($conf);
+print $hello->today();
+$form->login();
+$layout->footer($conf);
