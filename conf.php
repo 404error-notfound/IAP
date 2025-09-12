b0ea3cb5126d948f1e585eb75e78b1853734358a
Angela Omondi <omondiangela426@gmail.com>
2025-09-12 20:35:56 +0300

    Refactor layout and email functionality; implement new SendMail class and update layout structure

--- conf.php
+++ conf.php
@@ -2,14 +2,14 @@
 // Site Information
 $conf['site_name'] = 'ICS C Community';
 $conf['site_url'] = 'http://localhost';
-$conf['admin_email'] = 'admin@icsccommunity.com';
+$conf['admin_email'] = 'angela.omondi@strathmore.edu';
 
 // Database Configuration
 $conf['db_type'] = 'pdo';
 $conf['db_host'] = 'localhost';
 $conf['db_user'] = 'root';
-$conf['db_pass'] = '';
-$conf['db_name'] = 'nol';
+$conf['db_pass'] = 'whatdoyouthink';
+$conf['db_name'] = 'gopuppygo';
 
 // Site Language
 $conf['site_lang'] = 'en';
