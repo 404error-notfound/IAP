416f3f45e5f28809b6dd4874993f58af9f65946f
Angela Omondi <omondiangela426@gmail.com>
2025-09-12 21:24:24 +0300

    Refactor index and signin pages to use consistent object references; remove unused signup.php file

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php
-// Include the ClassAutoLoad Method
 require_once 'ClassAutoLoad.php';
-$layout->header($conf);
-print $hello->today();
-$form->signup();
-$layout->footer($conf);
+$ObjLayout->header($conf);
+$ObjForm->signup();
+$ObjLayout->footer($conf);
--- signin.php
+++ signin.php
@@ -1,7 +1,5 @@
 <?php
-// Include the ClassAutoLoad Method
 require_once 'ClassAutoLoad.php';
-$layout->header($conf);
-print $hello->today();
-$form->login();
-$layout->footer($conf);
+$ObjLayout->header($conf);
+$ObjForm->login();
+$ObjLayout->footer($conf);
